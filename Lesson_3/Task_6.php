fe170944078c6723ccbc3ad04bebff91e22698ca
andykey-krsk <andykey@bk.ru>
2020-06-17 15:33:58 +0700

    Урок №3. Задание 6

--- Lesson_3/Task_6.php
+++ Lesson_3/Task_6.php
@@ -5,5 +5,46 @@
 Важное, при желании можно сделать на движке 3. ВАЖНОЕ!*/
 
 $menu = [
-
+    ['url' => '/', 'name' => 'Главная'],
+    ['url' => '/catalog', 'name' => 'Каталог',
+        'sub_name' => [
+            ['url' => '/sub1', 'name' => 'Под меню 1'],
+            ['url' => '/sub2', 'name' => 'Под меню 2'],
+            ['url' => '/sub3', 'name' => 'Под меню 3']
+        ]],
+    ['url' => '/contacts', 'name' => 'Контакты',
+        'sub_name' => [
+            ['url' => '/sub1', 'name' => 'Под меню 1'],
+            ['url' => '/sub2', 'name' => 'Под меню 2'],
+            ['url' => '/sub3', 'name' => 'Под меню 3']
+        ]],
+    ['url' => '/about', 'name' => 'О нас']
 ];
+
+function getMenu($menu)
+{
+    $result = "<ul>";
+    foreach ($menu as $menuItem) {
+        $result .= getMenuItem($menuItem);
+    }
+    $result .= "</ul>";
+    return $result;
+}
+
+function getMenuItem($menuItem)
+{
+    $result = "<li>";
+    $sub_menu = "";
+    foreach ($menuItem as $key => $value) {
+        $$key = $value;
+        if (is_array($value)){
+            $sub_menu .= getMenu($value);
+        }
+    }
+        $result .= "<a href =" . $url . ">" . $name . "</a>" . $sub_menu;
+
+    $result .= "</li>";
+    return $result;
+}
+
+echo getMenu($menu);
