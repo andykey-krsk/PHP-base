080f4fbbf6f14efd9611acec4e3bc1d5926b5ae6
andykey-krsk <andykey@bk.ru>
2020-06-26 17:36:00 +0700

    Урок 5. задания 1-3

--- Lesson_5/GalleryToDB.php
+++ Lesson_5/GalleryToDB.php
@@ -1,1 +1,23 @@
 <?php
+define('ROOT', $_SERVER['DOCUMENT_ROOT']);
+define('PATH_BIG', ROOT . './gallery/big/');
+
+include_once "db.php";
+
+$pathToGallery = PATH_BIG;
+
+function makeGallery($pathToGallery)
+{
+    global $db;
+    $image_arr = array_slice(scandir($pathToGallery), 2);
+    mysqli_query($db, "INSERT INTO gallery(`gallery_filename`) VALUES ('" . implode("'),('", $image_arr) . "')");
+}
+
+$result = mysqli_query($db, "SELECT * FROM gallery");
+
+if ($result->num_rows == 0) {
+    makeGallery($pathToGallery);
+    echo "Данные добавлены в таблицу";
+} else {
+    echo "В таблице уже есть данные";
+}
--- Lesson_5/Task_1.php
+++ Lesson_5/Task_1.php
@@ -4,4 +4,49 @@
 просмотр конкретной фотографии (изображение оригинального размера)
 по ее ID в базе данных.*/
 
+define('PATH_BIG', './gallery/big/');
+define('PATH_SMALL', './gallery/small/');
+
 include_once "db.php";
+
+$arr_message = [
+    '1' => "Файл загружен",
+    '2' => "Ошибка закгузки файла",
+    '3' => "Неверный тип файла",
+    '4' => "Файл превышает максимально допустимый размер"
+];
+
+$message = $arr_message[$_GET['message']];
+
+$filename ="";
+
+if (isset($_POST['load'])) {
+
+    if ($_FILES["myfile"]["size"] > 2000000) {
+        header("Location: {$_SERVER['PHP_SELF']}?message=4");
+        die;
+    } elseif (exif_imagetype($_FILES["myfile"]["tmp_name"]) == IMAGETYPE_JPEG) {
+
+        $path_big = PATH_BIG . $_FILES["myfile"]["name"];
+        $path_small = PATH_SMALL . $_FILES["myfile"]["name"];
+
+        if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $path_big)) {
+
+            $filename = mysqli_real_escape_string($db, $_FILES["myfile"]["name"]);
+            mysqli_query($db, "INSERT INTO gallery(`gallery_filename`) VALUES ('$filename')");
+            $im = imagecreatefromjpeg($path_big);
+            $im = imagescale($im, 150, -1,);
+            imagejpeg($im, $path_small);
+            imagedestroy($im);
+            header("Location: {$_SERVER['PHP_SELF']}?message=1");
+        } else {
+            header("Location: {$_SERVER['PHP_SELF']}?message=2");
+        }
+    } else {
+        header("Location: {$_SERVER['PHP_SELF']}?message=3");
+    }
+}
+
+$image_arr = mysqli_query($db, "SELECT * FROM gallery ORDER BY gallery_view DESC");
+
+include "mygallery.php";
