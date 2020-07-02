7512b9c5afcc4d0c55eaefe31207594e5374df52
andykey-krsk <andykey@bk.ru>
2020-07-03 01:18:39 +0700

    Задания к уроку №7. Начало

---
+++ Lesson_7/engine/templates/menu.php
@@ -0,0 +1,6 @@
+<a href="/">Главная</a>
+<a href="/gallery/">Галерея</a>
+<a href="/catalog/">Каталог</a>
+<a href="/news/">Новости</a>
+<a href="/feedback/">Отзывы</a>
+<a href="/about/">О нас</a><br>
